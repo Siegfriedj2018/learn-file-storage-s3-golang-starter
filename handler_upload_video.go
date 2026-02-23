bd504b3795693f6abe59e431c7186e64b33007bb
Justin Siegfried <justasiegfried09@gmail.com>
2026-02-23 16:08:56 -0500

    updating aws course

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io"
@@ -8,6 +9,7 @@ import (
 	"mime"
 	"net/http"
 	"os"
+	"os/exec"
 	"path"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -102,22 +104,93 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	key := getAssetPath(mediaData)
 	key = path.Join(keyPrefix, key)
 
+	processedVideo, err := processVideoForFastStart(videoTemp.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "encountered an error processing video", err)
+		return
+	}
+	processFile, err := os.Open(processedVideo)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "encountered an error opening processed video", err)
+		return
+	}
+
 	params := &s3.PutObjectInput{
 		Bucket:      aws.String(cfg.s3Bucket),
 		Key:         aws.String(key),
-		Body:        videoTemp,
+		Body:        processFile,
 		ContentType: aws.String(mediaData),
 	}
 
 	_, err = cfg.s3Client.PutObject(context.TODO(), params)
 
-	videoUrl := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, key)
+	videoUrl := fmt.Sprintf("https://%v/%v", cfg.s3CfDistribution, key)
 	videoData.VideoURL = &videoUrl
 	err = cfg.db.UpdateVideo(videoData)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
 		return
 	}
-
+	// videoData, err = cfg.dbVideoToSignedVideo(videoData)
+	// if err != nil {
+	// 	respondWithError(w, http.StatusInternalServerError, "error encoding video", err)
+	// 	return
+	// }
 	respondWithJSON(w, http.StatusOK, videoData)
 }
+
+func processVideoForFastStart(filePath string) (string, error) {
+	processing := filePath + ".processing"
+
+	processFastStart := exec.Command(
+		"ffmpeg",
+		"-i",
+		filePath,
+		"-c", "copy",
+		"-movflags",
+		"faststart",
+		"-f", "mp4",
+		processing,
+	)
+	var stderr bytes.Buffer
+	processFastStart.Stderr = &stderr
+
+	err := processFastStart.Run()
+	if err != nil {
+		log.Printf("Command Log: %v", stderr.String())
+		log.Printf("Encountered an error processing video: %v", err)
+		return "", err
+	}
+
+	return processing, nil
+}
+
+// func (cfg *apiConfig) dbVideoToSignedVideo(video database.Video) (database.Video, error) {
+// 	if video.VideoURL == nil {
+// 		return video, nil
+// 	}
+// 	parts := strings.Split(*video.VideoURL, ",")
+// 	if len(parts) < 2 {
+// 		return video, nil
+// 	}
+// 	bucket := parts[0]
+// 	key := parts[1]
+// 	presigned, err := generatePresignedURL(cfg.s3Client, bucket, key, 5*time.Minute)
+// 	if err != nil {
+// 		return video, err
+// 	}
+// 	video.VideoURL = &presigned
+// 	return video, nil
+// }
+
+// func generatePresignedURL(s3Client *s3.Client, bucket, key string, expireTime time.Duration) (string, error) {
+// 	presignClient := s3.NewPresignClient(s3Client)
+// 	presignedUrl, err := presignClient.PresignGetObject(context.TODO(), &s3.GetObjectInput{
+// 		Bucket: aws.String(bucket),
+// 		Key:    aws.String(key),
+// 	}, s3.WithPresignExpires(expireTime))
+// 	if err != nil {
+// 		return "", fmt.Errorf("failed to generate presigned URL: %v", err)
+// 	}
+// 	return presignedUrl.URL, nil
+// }
